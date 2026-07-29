32da7fa4a0f6b5657ea6c658b7ae132e851f2052
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2026-07-29 10:59:12 -0400

    Fix collect_csvs call in run.sh not having $ before @

--- scripts/run.sh
+++ scripts/run.sh
@@ -33,4 +33,4 @@ julia --project="$PROJDIR" -e 'using LuxCUDA; CUDA.set_runtime_version!(v"12.9";
 make -C "$PROJDIR" "$@"
 
 # Collect individual trial CSV results into a single CSV file for the experiment
-julia --project="$PROJDIR" "$PROJDIR/scripts/collect_csvs.jl" "@"
+julia --project="$PROJDIR" "$PROJDIR/scripts/collect_csvs.jl" "$@"
